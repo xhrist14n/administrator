cc9e804a3500fe717a46ee5072985645db7f6340
khrist14n <khrist14n@gmail.com>
2016-08-16 01:03:07 -0500

    project administrator libraries

--- git.sh
+++ git.sh
@@ -1,6 +1,10 @@
 clear
 git add ./test
 git add ./app 
+git add ./app/scripts/. 
+git add ./app/styles/.
+git add ./app/images/.
+git add ./app/fonts/.
 git add bower.json
 git add generate_all.sh
 git add git.sh
